2c5cd7a45cab97f2c7d02d5be06c399a26e47a33
Joanna Grzywna <joanna.grzywna@gmail.com>
2025-12-01 14:04:03 +0100

    chore: improved argument parsing

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # AoC Solutions for 2025
+
+How to use:
+
+```
+usage: aoc.py [-h] [-t] day
+
+positional arguments:
+  day         Number of day
+
+options:
+  -h, --help  show this help message and exit
+  -t, --test  Run on test data
+```
+
+Example for running day 1 on test data
+
+```
+python aoc.py 1 -t
+```
+
+Example for running day 1 on final data
+
+```
+python aoc.py 1
+```
--- aoc.py
+++ aoc.py
@@ -1,9 +1,10 @@
 import sys
 import time
+import argparse
 
 def read_input(day, is_test):
     suffix = ""
-    if is_test == "True":
+    if is_test:
         suffix = "-test"
 
     with open(f"puzzles/day{day}/data{suffix}.txt", "r+") as dataFile:
@@ -23,16 +24,24 @@ def run_part(day, part, input):
 
     func_one(input) if part == 1 else func_two(input)
 
-def main(argv):
-    input_data = read_input(argv[0], argv[1])
+def main():
+
+    parser = argparse.ArgumentParser(description="AoC 2025 solutions")
+    parser.add_argument('day', type=int, help="Number of day")
+    parser.add_argument('-t', '--test', action="store_true", help="Run on test data")
+    args = parser.parse_args()
+
+    input_data = read_input(args.day, args.test)
+
+    print(f"{args}")
     
     start_time = time.time()
-    run_part(argv[0], 1, input_data)
+    run_part(args.day, 1, input_data)
     print("--- Solved in %s seconds ---\n" % (time.time() - start_time))
 
     start_time = time.time()
-    run_part(argv[0], 2, input_data)
+    run_part(args.day, 2, input_data)
     print("--- Solved in %s seconds ---\n" % (time.time() - start_time))
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
