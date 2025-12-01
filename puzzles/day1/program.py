ad845b487c6312d817396b0b0e07c031a3987894
Joanna Grzywna <joanna.grzywna@gmail.com>
2025-12-01 09:31:51 +0100

    chore: day1 setup

---
+++ puzzles/day1/program.py
@@ -0,0 +1,20 @@
+def execute_part_one(input: list[str]) -> None:
+    # count = 0
+
+    # for line in extract_data(input):
+    #     if is_safe_report(line):
+    #         count += 1
+
+    # print(f"Solved 1: {count}")
+    print(f"Solved 1")
+
+
+def execute_part_two(input: list[str]) -> None:
+    # count = 0
+
+    # for line in extract_data(input):
+    #     if is_safe_report(line):
+    #         count += 1
+
+    # print(f"Solved 2: {count}")
+    print(f"Solved 2:")
