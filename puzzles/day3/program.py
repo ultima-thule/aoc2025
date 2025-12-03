ea8ec91d2c09e79810f1cc967c55bac7ba280dd6
Joanna Grzywna <joanna.grzywna@gmail.com>
2025-12-03 12:21:11 +0100

    feat: day 3 part 1

--- puzzles/day3/program.py
+++ puzzles/day3/program.py
@@ -6,30 +6,38 @@ def extract_data(input: list[str]):
 
 
 def get_max_rating(bank):
-     if len(bank) < 2:
-          return 0
-     if len(bank) == 2:
-          return 10 * bank[0] + bank[1]
+    if len(bank) < 2:
+        return 0
      
-     i = 0
-     j = 2
+    if len(bank) == 2:
+        return 10 * bank[0] + bank[1]
 
-    max_voltage = 10 * bank[i] + bank[j]
+    idx_i = len(bank) - 2
+    idx_j = len(bank) - 1
 
-    while i < j:
-        tmp_voltage = 10 * bank[i] + bank[j]
-        while tmp_voltage > max_voltage and j < len(bank) - 1:
-             max_voltage = tmp_voltage
-             j += 1
+    max_i = bank[idx_i]
+    max_j = bank[idx_j]
 
+    for i in range (len(bank)-2, -1, -1):
+        # print(f"{i}: {bank[i]}, {bank[i+1]} (max: {max_i}, {max_j})")
+        if bank[i] >= max_i:
+            max_i = bank[i]
+            idx_i = i
+        
+    for j in range (len(bank)-1, idx_i, -1):
+        # print(f"{i}: {bank[i]}, {bank[i+1]} (max: {max_i}, {max_j})")
+        if bank[j] >= max_j:
+            max_j = bank[j]
+            idx_j = j
+
+    return 10 * max_i + max_j
      
 
 def execute_part_one(input: list[str]) -> None:
     count = 0
 
-    for line in extract_data(input):
-        print(line)
-        continue
+    for bank in extract_data(input):
+        count += get_max_rating(bank)
 
     print(f"Solved 1: {count}")
 
