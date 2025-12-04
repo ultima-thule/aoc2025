c6e78379a151bea419b1227c0335d06447ef67a0
Joanna Grzywna <joanna.grzywna@gmail.com>
2025-12-04 07:31:41 +0100

    feat: day 4 part 2

--- puzzles/day4/program.py
+++ puzzles/day4/program.py
@@ -21,45 +21,36 @@ def extract_data(input: list[str]):
     return ret, max_row, max_col
 
 
+def print_shelf(rolls, max_row, max_col):
+    # print(rolls)
+
+    print(f"{max_row} x {max_col}")
+    for r in range(max_row):
+        for c in range (max_col):
+            if (r, c) in rolls:
+                print("@", end='')
+            else:
+                print(".", end='')
+        print("\n")
+
+def check_if_can_remove(r, c, rolls, max_row, max_cols):
+    if (r, c) in rolls:
+        to_check = {(r-1, c), (r+1, c), (r, c-1), (r, c+1), (r-1, c-1), (r-1, c+1), (r+1, c-1), (r+1, c+1)}
+        if len(rolls & to_check) < 4:
+            return True
+
+    return False
+
+
 def execute_part_one(input: list[str]) -> None:
     count = 0
 
     rolls, max_row, max_col = extract_data(input)
 
-    # print(rolls)
-    # print(f"{max_row} x {max_col}")
-    # for r in range(max_row):
-    #     for c in range (max_col):
-    #         if (r, c) in rolls:
-    #             print("@", end='')
-    #         else:
-    #             print(".", end='')
-    #     print("\n")
-
     for r in range(max_row):
-
         for c in range (max_col):
-            cnt = 0
-            if (r, c) in rolls:
-                if (r-1, c) in rolls:
-                    cnt += 1
-                if (r+1, c) in rolls:
-                    cnt += 1
-                if (r, c-1) in rolls:
-                    cnt += 1
-                if (r, c+1) in rolls:
-                    cnt += 1
-                if (r-1, c-1) in rolls:
-                    cnt += 1
-                if (r-1, c+1) in rolls:
-                    cnt += 1
-                if (r+1, c-1) in rolls:
-                    cnt += 1
-                if (r+1, c+1) in rolls:
-                    cnt += 1
-                # print(f"{(c, r)} has {cnt}")
-                if cnt < 4:
-                    count += 1
+            if check_if_can_remove (r, c, rolls, max_row, max_col):
+                count += 1
 
     print(f"Solved 1: {count}")
 
@@ -67,5 +58,21 @@ def execute_part_two(input: list[str]) -> None:
     count = 0
 
     rolls, max_row, max_col = extract_data(input)
-    
+
+    while True:
+        removed = 0
+        rolls_to_remove = set()
+        for r in range(max_row):
+            for c in range (max_col):
+                if check_if_can_remove (r, c, rolls, max_row, max_col):
+                    removed += 1
+                    rolls_to_remove.add((r, c))
+        
+        rolls = rolls.difference(rolls_to_remove)
+        
+        count += removed
+        if removed == 0:
+            break
+
+
     print(f"Solved 2: {count}") 
