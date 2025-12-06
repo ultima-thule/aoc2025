bc8eae28b5ec17a31dbf212379649eb2566804a9
Joanna Grzywna <joanna.grzywna@gmail.com>
2025-12-06 07:21:24 +0100

    chore: day 6 structure

---
+++ puzzles/day6/program.py
@@ -0,0 +1,19 @@
+def extract_data(input: list[str]):
+    '''Extract and transform text data'''
+
+    data = []
+
+    for line in input:
+        continue
+       
+    return data
+
+def execute_part_one(input: list[str]) -> None:
+    count = 0
+
+    print(f"Solved 1: {count}")
+
+def execute_part_two(input: list[str]) -> None:
+    count = 0
+
+    print(f"Solved 2: {count}") 
